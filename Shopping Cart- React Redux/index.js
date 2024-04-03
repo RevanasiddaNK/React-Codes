4a5c5e6c89d5a7431ab33c5dd80577d6b3aa6486
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-04-03 22:33:16 +0530

    Add files via upload

---
+++ Shopping Cart- React Redux/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { store } from "./redux/Store";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store} >
+      <App />
+      <ToastContainer/>
+    </Provider>
+  </BrowserRouter>
+
+);
